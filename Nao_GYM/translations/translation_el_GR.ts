f5e83f51876cd33011716bb716e879897cf354c2
IoannisIn <intzes.ioannis@gmail.com>
2021-12-29 15:24:42 +0200

    Add files via upload

---
+++ Nao_GYM/translations/translation_el_GR.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el_GR">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Χαίρετε</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Είσαστε έτοιμοι?</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Χαίρετε</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Τέλεια!</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Hips intro 1/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Κοίταξε πως το κάνω!</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Hips intro 1/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Κούνησε τον γοφό σου όπως εγώ!</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Hips intro 1/Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Εύκολο, έτσι δεν είναι?</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Initialization/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Γεια σας παιδιά!</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Initialization/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Σήμερα θα κάνουμε μερικές ασκήσεις για να ξεμουδιάσουμε!</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ουφ!</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>κουράστηκα!</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 4/1</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Πάμε!</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 4/2</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>και</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 4/Init</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Έτοιμοι?</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
---
+++ Nao_GYM/translations/translation_en_US.ts
@@ -0,0 +1,392 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Γεια σας παιδιά!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Γεια σας παιδιά!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Είσαστε έτοιμοι?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Είσαστε έτοιμοι?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Σήμερα θα κάνουμε μερικές ασκήσεις για να ξεμουδιάσουμε!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Σήμερα θα κάνουμε μερικές ασκήσεις για να ξεμουδιάσουμε!</translation>
+        </message>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Ναι</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ναι</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Τέλεια!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Τέλεια!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Οχι</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Οχι</translation>
+        </message>
+        <message>
+            <source>Κρίμα!!!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Κρίμα!!!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>Τότε ας ξεκουραστώ και εγώ!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Τότε ας ξεκουραστώ και εγώ!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <source>Αντίο!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Αντίο!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (5)</name>
+        <message>
+            <source>Σιγουρα δεν θέλετε να κάνουμε γυμναστική?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Σιγουρα δεν θέλετε να κάνουμε γυμναστική?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arms intro 1/Say</name>
+        <message>
+            <source>Watch me first</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Watch me first</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arms intro 1/Say (1)</name>
+        <message>
+            <source>Move your hands up like this</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Move your hands up like this</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arms intro 1/Say (2)</name>
+        <message>
+            <source>Now like this</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Now like this</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Hips intro 1/Say</name>
+        <message>
+            <source>Check me out</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Check me out</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Κοίταξε πως το κάνω!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Κοίταξε πως το κάνω!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Hips intro 1/Say (1)</name>
+        <message>
+            <source>Roll your hip like this</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Roll your hip like this</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Κούνησε τον γοφό σου όπως εγώ!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Κούνησε τον γοφό σου όπως εγώ!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Hips intro 1/Say (3)</name>
+        <message>
+            <source>Easy, right?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Easy, right?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Εύκολο, έτσι δεν είναι?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Εύκολο, έτσι δεν είναι?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Initialization/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Γεια σας παιδιά!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Γεια σας παιδιά!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Initialization/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Σήμερα θα κάνουμε μερικές ασκήσεις για να ξεμουδιάσουμε!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Σήμερα θα κάνουμε μερικές ασκήσεις για να ξεμουδιάσουμε!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Legs intro 1/Left leg</name>
+        <message>
+            <source>MArch your legs like this</source>
+            <comment>Text</comment>
+            <translation type="obsolete">MArch your legs like this</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Legs intro 1/Right leg</name>
+        <message>
+            <source>and this</source>
+            <comment>Text</comment>
+            <translation type="obsolete">and this</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Legs intro 1/Say</name>
+        <message>
+            <source>Watch me first</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Watch me first</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Oxi/Animated Say (2)</name>
+        <message>
+            <source>Κρίμα!!!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Κρίμα!!!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Oxi/Animated Say (3)</name>
+        <message>
+            <source>Τότε ας ξεκουραστώ και εγώ!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Τότε ας ξεκουραστώ και εγώ!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Oxi/Animated Say (4)</name>
+        <message>
+            <source>Αντίο!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Αντίο!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Ουφ κουράστηκα!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ουφ κουράστηκα!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ουφ!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ουφ!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>κουράστηκα!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">κουράστηκα!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 1/1</name>
+        <message>
+            <source>ένα</source>
+            <comment>Text</comment>
+            <translation type="obsolete">ένα</translation>
+        </message>
+        <message>
+            <source>1</source>
+            <comment>Text</comment>
+            <translation type="obsolete">1</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 1/2</name>
+        <message>
+            <source>δύο</source>
+            <comment>Text</comment>
+            <translation type="obsolete">δύο</translation>
+        </message>
+        <message>
+            <source>2</source>
+            <comment>Text</comment>
+            <translation type="obsolete">2</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 1/Init</name>
+        <message>
+            <source>Παρακολούθησε την άσκηση μαζί μου σε τρία</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Παρακολούθησε την άσκηση μαζί μου σε τρία</translation>
+        </message>
+        <message>
+            <source>Follow the workout with me in 3</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Follow the workout with me in 3</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 2 intro/Say</name>
+        <message>
+            <source>Κοία εμένα πρώτα</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Κοία εμένα πρώτα</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 2 intro/Say (1)</name>
+        <message>
+            <source>Move your hands up like this</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Move your hands up like this</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 2 intro/Say (2)</name>
+        <message>
+            <source>Now like this</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Now like this</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 2/1</name>
+        <message>
+            <source>1</source>
+            <comment>Text</comment>
+            <translation type="obsolete">1</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 2/2</name>
+        <message>
+            <source>2</source>
+            <comment>Text</comment>
+            <translation type="obsolete">2</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 2/Init</name>
+        <message>
+            <source>Now follow me in 3</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Now follow me in 3</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 4/1</name>
+        <message>
+            <source>Go</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Go</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Πάμε!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Πάμε!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 4/2</name>
+        <message>
+            <source>Set</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Set</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>και</source>
+            <comment>Text</comment>
+            <translation type="unfinished">και</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout 4/Init</name>
+        <message>
+            <source>Okay, Ready</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Okay, Ready</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Έτοιμοι?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Έτοιμοι?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout intro 1/Left leg</name>
+        <message>
+            <source>Κούνησε το πόδι σου όπως εγώ!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Κούνησε το πόδι σου όπως εγώ!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout intro 1/Right leg</name>
+        <message>
+            <source>και το δεξί πόδι</source>
+            <comment>Text</comment>
+            <translation type="obsolete">και το δεξί πόδι</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Workout intro 1/Say</name>
+        <message>
+            <source>Κοίτα εμένα</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Κοίτα εμένα</translation>
+        </message>
+    </context>
+</TS>
